3c125148b11b6058dc1af0e9a786b323933e40ff
Omar Kraidié <omppula_1@hotmail.com>
2024-02-09 14:02:37 +0200

    Update default quote and author in App component

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ import { cn } from './lib/utils';
 
 function App() {
 	const [quoteData, setQuoteData] = useState({
-		quote: '',
-		author: '',
+		quote: 'When you arise in the morning, think of what a precious privilege it is to be alive - to breathe, to think, to enjoy, to love.',
+		author: 'Marcus Aurelius',
 		textColor: '#1B1B1B',
 		bgColor: '#FFFFFF',
 	});
